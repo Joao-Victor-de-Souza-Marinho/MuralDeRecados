5a944e84067495aaa71b11b95de1a1591d09758c
Joao-Victor-de-Souza-Marinho <177157496+Joao-Victor-de-Souza-Marinho@users.noreply.github.com>
2026-09-21 11:53:27 -0300

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,13 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetSocketAddress;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -80,5 +84,57 @@ public class Main {
         json.append("]");
         responder(troca,200,json.toString());
     }
+
+    private static Map<String, String> lerFormulario(HttpExchange troca) throws IOException{
+        String corpo = new String(
+                troca.getRequestBody().readAllBytes(), StandardCharsets.UTF_8
+        ) ;
+        //HashMap: classe que implementa o map
+        //Map = List e ArrayList = HashMap
+        Map<String, String> dados = new HashMap<>();
+        for (String campo: corpo.split("&")){
+            String[] partes = campo.split("=", 2);
+            String nome = URLDecoder.decode(partes[0], StandardCharsets.UTF_8);
+            String valor = partes.length == 2 ? URLDecoder.decode(partes[1], StandardCharsets.UTF_8): "";
+            //put: adicionar uma chave e um valor no Map
+            dados.put(nome,valor);
+        }
+        return dados;
+    }
+
+    private static void abrirPagina(HttpExchange troca) throws IOException{
+        if (!troca.getRequestMethod().equals("GET")){
+            responder(troca, 405, "Método não premitido", "text/plain");
+            return;
+        }
+        try(InputStream arquivo = Main.class.getResourceAsStream("/public/index.html")){
+            if (arquivo == null){
+                responder(troca, 404, "Pagina não encontrada", "text/plain");
+                return;
+            }
+            byte[] pagina = arquivo.readAllBytes();
+            troca.getResponseHeaders().set("Context-Type", "text/html; charset=UTF-8");
+            troca.sendResponseHeaders(200,pagina.length);
+            troca.getResponseBody().write(pagina);
+            troca.close();
+        }
+    }
+
+    private static void responder(HttpExchange troca, int status, String conteudo)
+        throws IOException{
+        responder(troca,status,conteudo, "application/json");
+    }
+
+    private static void responder(
+            HttpExchange troca, int status, String conteudo, String tipo
+    ) throws IOException{
+        byte[] resposta = conteudo.getBytes(StandardCharsets.UTF_8);
+        troca.getResponseHeaders().set("Context-Type", tipo + "; charset=UTF-8");
+        troca.sendResponseHeaders(status, resposta.length);
+        troca.getResponseBody().write(resposta);
+        troca.close();
+    }
+
+
 
 }
